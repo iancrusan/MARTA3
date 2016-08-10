b04924fc28dfbc04f2960626afd19cd6d5d4cca8
ian crusan <iancrusan@hotmail.com>
2016-08-10 13:48:38 -0400

    finally got buses to load now working on maps

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
-  include LocationsHelper
+  include ApplicationHelper
   # GET /locations
   # GET /locations.json
   def index
@@ -10,14 +10,14 @@ class LocationsController < ApplicationController
   # GET /locations/1
   # GET /locations/1.json
   def show
-    @buses = HTTParty.get("http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus")
+    @buses = HTTParty.get('http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus')
 
     @bus_count = 0
 
     @nearby_buses = []
 
     @buses.each do |bus|
-      if nearby(@location.longitude, @location.latitude, bus['LONGITUDE'].to_f, bus["LITITUDE"].to_f)
+      if nearby(@location.longitude, @location.latitude, bus["LONGITUDE"].to_f, bus["LATITUDE"].to_f)
         @bus_count += 1
         @nearby_buses.push(bus)
       end
